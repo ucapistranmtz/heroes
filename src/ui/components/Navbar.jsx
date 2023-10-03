e6eed90e12b18ad79008ab9c1c6b7e6f0834b4d7
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-03 12:47:32 -0500

    lesson 212 end

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
-
+import {  AuthContext} from  '../../auth/context'
 
 export const Navbar = () => {
+  
+     const {user} = useContext(AuthContext);
   
     const navigate = useNavigate();
 
@@ -55,7 +58,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Ulises
+                         { user?.name}
                     </span>
                     <button className="nav-item nav-link btn" onClick={onLogOut}>
                         LogOut
