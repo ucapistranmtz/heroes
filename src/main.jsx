fbc62b1bc8427a9d0cce7cddf2ae1d312d981e44
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-03 15:18:54 -0500

    fix again

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
- 
+
 import './styles.css'
-import {HeroesApp} from "./HeroesApp.jsx"
+import { HeroesApp } from "./HeroesApp.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <HeroesApp />
     </BrowserRouter>
-   
-  </React.StrictMode>,
+  </React.StrictMode>
 )
