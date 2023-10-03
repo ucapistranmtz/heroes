1085c12b91aab4abb21dcc98f05a7a9f079e87f5
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-03 14:01:59 -0500

    lesson 215

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,24 @@
- import {Routes,Route} from "react-router-dom"
- import { LoginPage } from "../auth/pages/LoginPage"
- 
- import {HeroesRoutes} from '../heroes'
+import { Routes, Route } from "react-router-dom"
+import { LoginPage } from "../auth/pages/LoginPage"
+
+import { HeroesRoutes } from '../heroes'
+import { PrivateRoute } from "./PrivateRoute"
 
 export const AppRouter = () => {
   return (
     <>
-     
-     <Routes> 
-        <Route path="/login" element={<LoginPage/>}/> 
-        <Route path="/*" element={<HeroesRoutes/>}/> 
-     </Routes>
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroesRoutes></HeroesRoutes>
+          </PrivateRoute>
+        } />
+
+      </Routes>
     </>
-  
+
   )
 }
