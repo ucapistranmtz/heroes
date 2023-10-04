253b20d774f602bfa7719eb796e265ec69a45345
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-04 06:21:27 -0500

    lesson 226

--- test/router/PublicRoute.test.jsx
+++ test/router/PublicRoute.test.jsx
@@ -1,8 +1,9 @@
 
 
-import {  render,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { AuthContext } from "../../src/auth";
 import { PublicRoute } from "../../src/router/PublicRoute";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 describe('public route tests', () => {
     test('should show the children if not authenticate', () => {
@@ -17,9 +18,39 @@ describe('public route tests', () => {
                     <h1>Public Route</h1>
                 </PublicRoute>
             </AuthContext.Provider>
-        ); 
+        );
 
         expect(screen.getByText('Public Route')).toBeTruthy();
 
     });
+
+    test('should be able to browse if the user is authenticated', () => {
+
+        const contexValue = {
+            logged: true,
+            user: {
+                name: 'Jesus',
+                id: 'ABCD123'
+            }
+        }
+
+        render(
+            <AuthContext.Provider value={contexValue}>
+                <MemoryRouter initialEntries={['/login']}>
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1>Public Route</h1>
+                            </PublicRoute>
+                        } />
+                        <Route path='marvel' element={<h1>Marvel Page</h1>} />
+                    </Routes>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        //screen.debug();
+
+        expect(screen.getByText('Marvel Page')).toBeTruthy();
+    })
 });
